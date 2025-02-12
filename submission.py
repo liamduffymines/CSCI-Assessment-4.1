44e78dc62b61a0296f518ef2dd4ba2aa33712ae1
liamduffymines <liam_duffy@mines.edu>
2025-02-11 22:52:53 -0700

    Create submission.py

---
+++ submission.py
@@ -0,0 +1,49 @@
+#Liam Duffy 
+#CSCI 128
+
+
+
+Gate = input("Gate> ")
+INP1 = int(input("INP1> "))
+INP2 = int(input("INP2> "))
+
+Gates_List = ["AND", "OR", "NAND", "NOR", "XOR", "XNOR"]
+
+if Gate not in Gates_List:
+    print((f"OUTPUT Invalid Input {Gate}"))
+
+elif INP1 != 0 and INP1 != 1:
+    print(f"OUTPUT Invalid Input {INP1}")
+
+elif INP2 != 0 and INP2 != 1: 
+    print("OUTPUT Invalid Input {INP2}")
+
+else:
+    if INP1 == 0: 
+        INP1 = False
+    
+    else: 
+        INP1 = True
+    
+    if INP2 == 0: 
+        INP2 = True
+    
+    else: INP2 = False
+    
+    if Gate == "AND":
+        print(f"OUTPUT {INP1 and INP2}")
+    
+    elif Gate == "OR":
+        print(f"OUTPUT{INP1 or INP2}")
+    
+    elif Gate == "NAND":
+        print({not(INP1 and INP2)})
+    
+    elif Gate == "NOR":
+        print(f"OUTPUT (not{INP1 or INP2})")
+    
+    elif Gate == "XOR":
+        print(f"OUTPUT {not(INP1 ^ INP2)})")
+    
+    elif Gate == "XNOR":
+         print(f"OUTPUT{not(INP1 ^ INP2)}")
