23fe698c99565fb3b730709ee2faf3faf19b4ea5
liamduffymines <liam_duffy@mines.edu>
2025-02-11 23:06:44 -0700

    submission.py

--- submission.py
+++ submission.py
@@ -10,40 +10,56 @@ INP2 = int(input("INP2> "))
 Gates_List = ["AND", "OR", "NAND", "NOR", "XOR", "XNOR"]
 
 if Gate not in Gates_List:
-    print((f"OUTPUT Invalid Input {Gate}"))
+    print(f"OUTPUT Invalid Gate {Gate}")
 
 elif INP1 != 0 and INP1 != 1:
     print(f"OUTPUT Invalid Input {INP1}")
 
 elif INP2 != 0 and INP2 != 1: 
-    print("OUTPUT Invalid Input {INP2}")
-
+    print(f"OUTPUT Invalid Input {INP2}")
 else:
     if INP1 == 0: 
         INP1 = False
-    
     else: 
         INP1 = True
     
     if INP2 == 0: 
-        INP2 = True
-    
-    else: INP2 = False
+        INP2 = False
+    else: INP2 = True
     
     if Gate == "AND":
         print(f"OUTPUT {INP1 and INP2}")
     
     elif Gate == "OR":
-        print(f"OUTPUT{INP1 or INP2}")
+        print(f"OUTPUT {INP1 or INP2}")
     
     elif Gate == "NAND":
-        print({not(INP1 and INP2)})
+        print(not{INP1 and INP2})
     
     elif Gate == "NOR":
         print(f"OUTPUT (not{INP1 or INP2})")
     
     elif Gate == "XOR":
-        print(f"OUTPUT {not(INP1 ^ INP2)})")
+        print(f"OUTPUT {(INP1 ^ INP2)}")
     
     elif Gate == "XNOR":
          print(f"OUTPUT{not(INP1 ^ INP2)}")
+   
+   
+    
+    
+  
+   
+    
+    
+   
+
+
+
+
+
+
+
+
+
+    
